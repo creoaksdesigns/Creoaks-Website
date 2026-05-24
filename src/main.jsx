fc6d5957607a5047a14a59658dd514e50efd7bf7
creoaksdesigns <ejiroben70@gmail.com>
2026-05-24 15:22:26 +0100

    .

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>>
+    <HelmetProvider>
       <App />
     <HelmetProvider
-  </StrictMode>,
+  </StrictMode>
 )
